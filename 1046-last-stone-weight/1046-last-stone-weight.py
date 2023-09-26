77d68e98e14f675bc11e726763bf4356710bf7d8
Juhee0101 <132970380+Juhee0101@users.noreply.github.com>
2023-09-26 18:21:53 +0900

    Time: 45 ms (17.49%), Space: 16.3 MB (59.94%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        if len(stones) == 0:
+            return 0
+    
+        while (len(stones) > 1):
+            s1 = max(stones)
+            stones.remove(s1)
+
+            s2 = max(stones)
+            stones.remove(s2)
+
+            newStone = abs(s1 - s2)
+            if newStone > 0:
+                stones.extend([newStone])
+
+        if len(stones) != 0:
+            return stones[0]
+        else:
+            return 0
